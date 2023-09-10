8eff9b1500d5e417fed0cb822cb8b81e008b7da1
acaldas <acaldas@powerhouse.inc>
2023-09-10 20:57:57 +0100

    Initial version with doca assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,13 @@ export default {
       transitionTimingFunction: {
         DEFAULT: defaultTheme.transitionTimingFunction.out,
       },
+      colors: {
+        green: "#39656D",
+        blue: "#ADC5D8",
+        yellow: "#F8DDA0",
+        background: "#FCF5EB",
+        text: "#262E38",
+      },
     },
   },
 } satisfies Config;
