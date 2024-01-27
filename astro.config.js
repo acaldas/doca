6c7e1b429b0c55c86693fc2b7bf1f6cfd8515206
acaldas <acaldas@powerhouse.inc>
2024-01-27 13:19:06 +0000

    Added sitemap and robots.txt

--- astro.config.js
+++ astro.config.js
@@ -2,14 +2,20 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind({
-    applyBaseStyles: false
-  }), react()],
+  integrations: [
+    mdx(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    sitemap(),
+  ],
+  site: "https://www.equipadoca.pt/",
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
